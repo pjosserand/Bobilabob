2e391b064f983446247e8dbbbde6c3ec34567ba5
Elvyn Wakeford <elvynwakeford@gmail.com>
2022-04-15 13:06:25 +0200

    player run animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,48 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(NavMeshAgent))]
 public class PlayerController : MonoBehaviour
 {
-
     public int lifePoints;
-    
-    [Range(1f, 50f)]
-    [SerializeField] private float _rayMaxDistance = 20f;
+
+    [Range(1f, 50f)] [SerializeField] private float _rayMaxDistance = 20f;
 
     [SerializeField] LayerMask _groundLayer;
 
     private Camera _mainCamera;
     private NavMeshAgent _agent;
+    public Animator _animator;
 
     void Start()
     {
-        _mainCamera = Camera.main;    
+        _mainCamera = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
     }
 
-    void OnRightClick()
+    private void Update()
+    {
+        _animator.SetFloat("Velocity", Math.Abs(_agent.velocity.x + _agent.velocity.z));
+    }
+
+    void OnRightClick(InputValue prminput)
+    {
+        if (prminput.isPressed)
+        {
+           InvokeRepeating(nameof(move), 0f, 0.2f );
+        }
+        else
+        {
+            CancelInvoke();
+        }
+    }
+
+    void move()
     {
         Ray cameraRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        
+
         RaycastHit hitInfo;
         if (Physics.Raycast(cameraRay, out hitInfo, _rayMaxDistance, _groundLayer.value))
         {
@@ -41,12 +59,10 @@ public class PlayerController : MonoBehaviour
         {
             Death();
         }
-        
     }
 
     void Death()
     {
         //DeathStuff
     }
-    
 }
